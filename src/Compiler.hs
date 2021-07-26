77d68e6b5b4f75b5447332923a641a337c10889c
9nine9nine9 <J.Walters-6@sms.ed.ac.uk>
2021-07-26 01:55:39 +0100

    Interpreter complete!

--- src/Compiler.hs
+++ src/Compiler.hs
@@ -1,6 +1,7 @@
 module Compiler where
 
 import Control.Applicative ((<|>), some)
+import Data.Char
 
 import Types
 import Parser
@@ -20,8 +21,8 @@ parseInstr = foldl1 (<|>)
     , ShiftR <$ char '>'
     , Inc    <$ char '+'
     , Dec    <$ char '-'
-    , Print  <$ char ','
-    , Read   <$ char '.'
+    , Read   <$ char ','
+    , Print  <$ char '.'
     , Loop   <$>
         (char '[' *>
          some parseInstr
@@ -33,3 +34,43 @@ parseProg = some parseInstr <* eof
 parse :: String -> Maybe Prog
 parse str = snd <$> runParser parseProg str
 
+
+-- INTERPRETER STUFF
+shiftL, shiftR :: Tape a -> Tape a
+shiftL t@(Tape [] b cs)   = t
+shiftL (Tape (a:as) b cs) = Tape as a (b:cs)
+shiftR t@(Tape as b [])   = t
+shiftR (Tape as b (c:cs)) = Tape (b:as) c cs
+
+alterCell :: (a -> a) -> Tape a -> Tape a
+alterCell f (Tape as b cs) = Tape as (f b) cs
+
+getCell :: Tape a -> a
+getCell (Tape _ a _) = a
+
+setCell :: a -> Tape a -> Tape a
+setCell = alterCell . const
+
+-- | Interprets a program in a given memory state
+runBF :: Mem -> Prog -> IO Mem
+runBF mem [] = return mem
+runBF mem (i:is) = do
+    case i of
+         ShiftL  -> runBF (shiftL            mem) is
+         ShiftR  -> runBF (shiftR            mem) is
+         Inc     -> runBF (alterCell (+1)    mem) is
+         Dec     -> runBF (alterCell (+(-1)) mem) is
+         Print   -> do
+             putChar $ chr $ getCell mem
+             runBF mem is
+         Read    -> do
+             c <- getChar
+             runBF (setCell (ord c) mem) is
+         Loop is' -> case getCell mem of
+                         0 -> runBF mem is
+                         _ -> runBF mem is' >>= flip runBF (i:is)
+
+
+
+helloworld :: String
+helloworld = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.<<"
--- src/Types.hs
+++ src/Types.hs
@@ -1,7 +1,10 @@
 module Types where
 
 data Tape a = Tape [a] a [a]
-    deriving Show
+instance Show a => Show (Tape a) where
+    show (Tape as b cs) = show [reverse as, [b], cs]
+
+type Mem = Tape Int
 
 data Instr = Loop [Instr] -- [...]
            | ShiftL       -- <
